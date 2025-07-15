8b68bbc1e6ee779fbe1a4709426dd00e928cc20a
DiegoAleContre <soydiego438@gmail.com>
2025-07-15 17:08:14 -0400

    Nueva Base de Datos

--- travely-db.sql
+++ travely-db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-07-2025 a las 01:31:52
+-- Tiempo de generación: 15-07-2025 a las 23:05:18
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.0
 
@@ -24,6 +24,38 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `pasajeros`
+--
+
+CREATE TABLE `pasajeros` (
+  `id` int(11) NOT NULL,
+  `id_pasajero1` int(11) NOT NULL,
+  `id_pasajero2` int(11) NOT NULL,
+  `id_pasajero3` int(11) NOT NULL,
+  `id_pasajero4` int(11) NOT NULL,
+  `id_pasajero5` int(11) NOT NULL,
+  `id_pasajero6` int(11) NOT NULL,
+  `id_pasajero7` int(11) NOT NULL,
+  `id_pasajero8` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ruta`
+--
+
+CREATE TABLE `ruta` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `salida` varchar(100) NOT NULL,
+  `llegada` varchar(100) NOT NULL,
+  `precio` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -32,19 +64,33 @@ CREATE TABLE `usuarios` (
   `nombre` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `tipo` enum('conductor','pasajero','admin',) NOT NULL,
+  `tipo` enum('conductor','pasajero','admin') NOT NULL,
   `sexo` enum('masculino','femenino','otro') DEFAULT NULL,
-  `creado_en` timestamp NOT NULL DEFAULT current_timestamp()
+  `creado_en` timestamp NOT NULL DEFAULT current_timestamp(),
+  `estado` enum('activo','inactivo') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `email`, `password`, `tipo`, `sexo`, `creado_en`) VALUES
-(1, 'admin', 'admin@travely.com', '$2b$10$tFy4nb4KKzfRNMeg/O6FWeK6BoLtVgLIhJwj/wnyaqg.525tn862q', 'admin', 'femenino', '2025-06-28 23:57:54'),
-(2, 'Marcods', 'qwdqw@afae', '$2b$10$tFy4nb4KKzfRNMeg/O6FWeK6BoLtVgLIhJwj/wnyaqg.525tn862q', 'pasajero', 'femenino', '2025-06-28 23:57:54'),
-(3, 'Diego', 'contrerasdiego8718@gmail.com', '$2b$10$RfLt8fJLrc9s2KBr7Tl9MuqHZg/priEVKTbCkCad8qGvMtE4MCK6a', 'conductor', 'masculino', '2025-07-05 19:24:23');
+INSERT INTO `usuarios` (`id`, `nombre`, `email`, `password`, `tipo`, `sexo`, `creado_en`, `estado`) VALUES
+(1, 'admin', 'admin@travely.com', '$2b$10$1/k45Ct6kHZyCxxO1P9Sj.A3of5vg0ZczzRuM82eBtF6YaR9CW2C.', 'admin', 'otro', '2025-07-15 21:04:36', 'activo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vehiculo`
+--
+
+CREATE TABLE `vehiculo` (
+  `id` int(11) NOT NULL,
+  `tipo` enum('carro','moto','camioneta','bus') NOT NULL,
+  `modelo` varchar(100) NOT NULL,
+  `marca` varchar(100) NOT NULL,
+  `color` varchar(80) NOT NULL,
+  `capacidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -54,26 +100,32 @@ INSERT INTO `usuarios` (`id`, `nombre`, `email`, `password`, `tipo`, `sexo`, `cr
 
 CREATE TABLE `viajes` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(250) NOT NULL,
-  `vehiculo` varchar(250) NOT NULL,
-  `asientos` int(11) NOT NULL,
-  `ruta` enum('Valera','Carvajal','Centro-Valera','Country','La Beatriz','Trujillo') NOT NULL,
-  `precio` int(11) NOT NULL,
-  `disponibleHoy` enum('true','false') NOT NULL,
-  `creandoEn` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `descripcion` varchar(250) NOT NULL,
+  `id_vehiculo` int(11) NOT NULL,
+  `asientos_disp` int(10) NOT NULL,
+  `id_ruta` int(11) NOT NULL,
+  `disp_hoy` tinyint(1) NOT NULL DEFAULT 0,
+  `inicia_el` datetime NOT NULL,
+  `creado_en` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `id_conductor` int(11) NOT NULL,
+  `id_pasajeros` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `viajes`
---
-
-INSERT INTO `viajes` (`id`, `nombre`, `vehiculo`, `asientos`, `ruta`, `precio`, `disponibleHoy`, `creandoEn`) VALUES
-(1, 'Prueba ya nose', 'Moto', 2, 'Carvajal', 45, '', '2025-07-05 22:27:35'),
-(2, '234234', 'Moto', 43, 'Carvajal', 43, '', '2025-07-05 22:58:15');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `pasajeros`
+--
+ALTER TABLE `pasajeros`
+  ADD PRIMARY KEY (`id`);  
+
+--
+-- Indices de la tabla `ruta`
+--
+ALTER TABLE `ruta`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -83,26 +135,61 @@ ALTER TABLE `usuarios`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indices de la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `viajes`
 --
 ALTER TABLE `viajes`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_vehiculo` (`id_vehiculo`,`id_ruta`,`id_conductor`,`id_pasajeros`),
+  ADD KEY `id_pasajeros` (`id_pasajeros`),
+  ADD KEY `id_conductor` (`id_conductor`),
+  ADD KEY `id_ruta` (`id_ruta`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `pasajeros`
+--
+ALTER TABLE `pasajeros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `viajes`
 --
 ALTER TABLE `viajes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `viajes`
+--
+ALTER TABLE `viajes`
+  ADD CONSTRAINT `viajes_ibfk_1` FOREIGN KEY (`id_vehiculo`) REFERENCES `vehiculo` (`id`),
+  ADD CONSTRAINT `viajes_ibfk_2` FOREIGN KEY (`id_pasajeros`) REFERENCES `pasajeros` (`id`),
+  ADD CONSTRAINT `viajes_ibfk_3` FOREIGN KEY (`id_conductor`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `viajes_ibfk_4` FOREIGN KEY (`id_ruta`) REFERENCES `ruta` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
